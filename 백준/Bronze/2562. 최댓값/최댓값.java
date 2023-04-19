196f7dcdbf89f684a678f0dca27bc30b3d55ad17
Dong Hyeok <dong3405@gmail.com>
2023-04-20 03:44:06 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] array = new int[9];
+
+		int max = array[0];
+		int index = 0;
+
+		for (int i = 0; i < 9; i++) {
+			array[i] = sc.nextInt();
+			if (max < array[i]) {
+				max = array[i];
+				index = i + 1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(index);
+		sc.close();
+
+	}
+
+}
