42b55414cf1aad261adc88af16cfeda4eb1f1694
Dong Hyeok <dong3405@gmail.com>
2023-04-25 12:48:52 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int j = num_list.length;
+        for(int i = 0; i<num_list.length; i++){
+            answer[i] = num_list[j-1];
+            j--;
+        }
+        return answer;
+    }
+}
