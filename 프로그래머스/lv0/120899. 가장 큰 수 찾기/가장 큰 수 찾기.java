2881032391a5282cae3b2f32a1a6aba5ed72b2bc
Dong Hyeok <dong3405@gmail.com>
2023-05-02 20:40:56 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int max = 0;
+        
+        for(int i = 0; i<array.length; i++){
+            if(max<array[i]){
+                max = array[i];
+                answer[0] = array[i];
+                answer[1] = i; 
+            }
+        }
+        
+        return answer;
+    }
+}
