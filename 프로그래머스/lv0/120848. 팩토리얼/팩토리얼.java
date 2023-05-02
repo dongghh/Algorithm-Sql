b6df3d040febdcd6a7ce6beb496da22f976ef869
Dong Hyeok <dong3405@gmail.com>
2023-05-02 17:16:28 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = 1;
+        
+        for(int i = 1; i<100; i++){
+            num *= i ;
+            if(num == n){
+                answer = i;
+                break;
+            }else if(num > n){
+                answer = i - 1;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
