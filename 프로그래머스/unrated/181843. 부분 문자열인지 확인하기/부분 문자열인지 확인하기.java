b797af0ea20c28f1263e61dfcbdee9be3c2aa189
Dong Hyeok <dong3405@gmail.com>
2023-05-10 22:14:57 +0900

    [unrated] Title: 부분 문자열인지 확인하기, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String str, String target) {
+        int answer = 0;
+        if(str.contains(target)){
+            return answer =1;
+        }
+        return answer;
+    }
+}
