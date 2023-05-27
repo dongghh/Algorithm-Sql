f4aa6f55de2b1c258f1110ce01fb9366bd0a8b1d
Dong Hyeok <dong3405@gmail.com>
2023-05-27 21:53:06 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        
+        for(int i = 0; i<answer.length-n; i++){
+            answer[i] = num_list[i+n];
+        }
+        
+        for(int i = 0; i<n; i++){
+            answer[answer.length-n+i] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
